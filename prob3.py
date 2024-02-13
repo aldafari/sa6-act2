ac568805961319208150ac1893e80ecb675e1722
aldafari <aldafari@memphis.edu>
2024-02-13 17:20:43 -0600

    Made correction to prob3

--- prob3.py
+++ prob3.py
@@ -1,4 +1,4 @@
 people = [("Alice", 30), ("Bob", 25), ("Charlie", 35)]
 # Use a lambda function as the sorting key
-sorted_people = sorted(people, key=lambda x: people[1])
-print(sorted_people)
+people.sort(key=lambda people: people [1])
+print(people)
